a9c4420c6b5b3666a05b1bf556b32adb2ae89f29
Esteban Jaramillo <ejaramillopalma@hotmail.com>
2025-02-10 22:16:49 -0300

    add minor fixes and frontend test api

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.database import engine, Base
 from app.api.endpoints import citas
 
 from app.models.models import Paciente, Medico, Cita
 
+# Crear las tablas en la base de datos
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Configuración del middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todos los orígenes. Cambiar a dominios específicos en producción.
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos HTTP
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
+
+# Registrar el router de citas
 app.include_router(citas.router)
 
 if __name__ == "__main__":
